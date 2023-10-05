78e1840986a14c78b382b6dac870e5cf8ae35484
Emma Coppin <emmacoppin@MacBook-Pro-de-Emma.local>
2023-10-05 13:08:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import functions as fct
+import math
 
 # Visualisation de la structure initiale
 nodes, elements = fct.read_data('init_nodes.txt')
@@ -27,5 +28,11 @@ locel = []
 for i in range(len(elements)) :    
     locel.append([dof_list[elements[i][0]][0],dof_list[elements[i][0]][1], dof_list[elements[i][0]][2], dof_list[elements[i][0]][3], dof_list[elements[i][0]][4], dof_list[elements[i][0]][5],
                   dof_list[elements[i][1]][0], dof_list[elements[i][1]][1], dof_list[elements[i][1]][2], dof_list[elements[i][1]][3], dof_list[elements[i][1]][4], dof_list[elements[i][1]][5]])
+
+
+# Définition des constantes 
+E = 210e6                             # Module de Young [Pa]
+A_leg = math.pi*(1 - 0.02**2)         # Section d'une poutre principale [m^2]
+A_beam = math.pi*(0.6**2 - 0.02**2)   # Section d'une poutre secondaire [m¨2]
 
 

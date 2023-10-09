54ebfb606848fbf652c7c2fc08a78635aded448a
Emma Coppin <emmacoppin@MacBook-Pro-de-Emma.local>
2023-10-09 17:01:55 +0200

    voila voila

--- functions.py
+++ functions.py
@@ -133,13 +133,13 @@ def euclidian_distance(elem, elements, nodes) :
 def elem_matrix(beam_param) : 
     """ Crée les matrices élémentaires
         Arguements : 
-            - beam_param : une array contenant les paramètres de la poutre : [A, r, h, E, Iz, Iy, Jx, G]
+            - beam_param : une array contenant les paramètres de la poutre : [A, r, h, E, Iz, Iy, Jx, G, rho]
         Return : 
             - M_el : la matrice élémentaire de masse
             - K_el : la matrice élémentaire d'énergie cinétique
     """
-    A = beam_param[0]; r = beam_param[1]; h=beam_param[2]; E=beam_param[3]; Iz=beam_param[4]; Iy=beam_param[5]; Jx=beam_param[6]; G=beam_param[7]
-    r = 1
+    A = beam_param[0]; r = beam_param[1]; h=beam_param[2]; E=beam_param[3]; Iz=beam_param[4]; Iy=beam_param[5]; Jx=beam_param[6]; G=beam_param[7]; rho=beam_param[8]
+    #r = 1
     a = (11*h)/210
     b = (13*h)/420
     c = (h**2)/105
@@ -157,7 +157,8 @@ def elem_matrix(beam_param) :
             [    0,     0,     0,   d/2,     0,     0,     0,     0,     0,     d,     0,     0],
             [    0,     0,     b,     0,    -e,     0,     0,     0,     a,     0,     c,     0],
             [    0,    -b,     0,     0,     0,    -e,     0,    -a,     0,     0,     0,     c]]
-    
+    M_el = M_el*rho*A*h
+
     f = (E*A)/h
     g = (12*E*Iz)/(h**3)
     i = (6*E*Iz)/(h**2)
@@ -178,6 +179,65 @@ def elem_matrix(beam_param) :
             [    0,     0,     0,    -m,     0,     0,     0,     0,     0,     m,     0,     0],
             [    0,     0,    -k,     0,     o,     0,     0,     0,     k,     0,   2*o,     0],
             [    0,     i,     0,     0,     0,     n,     0,    -i,     0,     0,     0,   2*n]]
+
     
     return M_el, K_el
     
+def get_param(elem, leg_elem, rili_elem, elements, nodes) : 
+    """ Crée la liste des paramètres en fonction de la catégorie de l'élément (leg, rigid link or beam)
+        Arguments : 
+            - elem : l'index de l'éléments concerné
+            - leg_elem : la liste d'index des éléments appartenants aux legs
+            - rili_elem : la liste d'index des éléments appartenants aux rigid links
+            - elements : la liste ldes éléments
+            - nodes : la liste des noeuds
+        Return : 
+            - param : la liste des paramètres de l'élément : [A, r, h, E, Iz, Iy, Jx, G, rho]
+    """
+
+    # Définition des constantes pour la structure
+    E = 210e6                                           # Module de Young [Pa]
+    A_leg = math.pi*(1 - (1-0.02)**2)                   # Section d'une poutre principale [m^2]
+    A_beam = math.pi*(0.6**2 - (0.6-0.02)**2)           # Section d'une poutre secondaire [m¨2]
+    nu = 0.3                                            # Coefficient de Poisson [-]
+    rho = 7800                                          # Densité de l'acier utilisé [kg/m^3]
+    G = E/(2*(1+nu))                                    # Module de cisaillement [Pa]
+    h = euclidian_distance(elem, elements,nodes)
+    h = h*1e-3
+
+    param = []
+
+    if elem in leg_elem : 
+        # Définition des caractéristiques pour une pour principale (elon les axes locaux)
+        m_leg = rho*math.pi*h*(1**2 - (1-0.02)**2)                  # Masse d'une poutre principale [kg]
+        Iyz_leg = (math.pi/4)*(1**4 - (1-0.02)**2)                  # Moment quadratique selon l'axe y et z [m^4]
+        Ix_leg = (math.pi/2)*(1**4 - (1-0.02)**2)                   # Moment quadratique selon l'axe x [m^4]
+        Jx_leg = 0.5*m_leg*(1**2 + (1-0.02)**2)                     # Moment d'inertie selon l'axe x [kg.m^2]
+        Jyz_leg = 0.25*m_leg*(1**2 + (1-0.02)**2)+(m_leg*(h**2))/12 # Moment d'inertie selon l'axe y et z [kg.m^2]
+
+        param = [A_leg, (1.0 + (1-0.02))/2, h, E, Iyz_leg, Iyz_leg, Jx_leg, G, rho]
+    if elem in rili_elem : 
+        # Définition des constantes pour les rigid links
+        rho_r = rho*1e-4    # Densité [kg/m^3]
+        E_r = E*1e4         # Module de Young [Pa]
+        A_r = A_leg*1e-2    # Section [m^2]
+        Iyz_r = Iyz_leg*1e4 # Moment quadratique selon l'axe y et z [m^4]
+        Jx_r = Jx_leg*1e4   # Moment d'intertie selon l'axe x [kg.m^2]
+        G_r = E_r/(2*(1+nu))# Module de cisaillement [Pa]
+        r = math.sqrt(A_r/math.pi) # A VERIFIER
+
+        param = [A_r, r, h, E_r, Iyz_r, Iyz_r, Jx_r, G_r, rho_r]
+
+    else : 
+        # Définition des caractéristiques pour une poutre secondaire (selon les axes locaux)
+        m_beam = rho*math.pi*h*(0.6**2 - (0.6-0.02**2))                    # Masse [kg]
+        Iyz_beam = (math.pi/4)*(0.6**4 - (0.6-0.02)**2)                    # Moment quadratique selon l'axe y et z [m^4]
+        Ix_beam = (math.pi/2)*(0.6**4 - (0.6-0.02)**2)                     # Moment quadratique selon l'axe x [m^4]
+        Jx_beam = 0.5*m_beam*(0.6**2 + (0.6-0.02)**2)                      # Moment d'inertie selon l'axe x [kg.m^2]
+        Jyz_beam = 0.25*m_beam*(0.6**2 + (0.6-0.02)**2)+(m_beam*(h**2))/12 # Moment d'inertie selon l'axe y et z [km.m^2]
+
+        param = [A_beam, (0.6+(0.6-0.02))/2, h, E, Iyz_beam, Iyz_beam, Jx_beam, G, rho]
+
+    return param
+
+
--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for i in range(len(nodes)) :
 # Création de la matrice locel (reprenant les dof impliqués pour chaque élément)
 locel = []
 for i in range(len(elements)) :    
-    locel.append([dof_list[elements[i][0]][0],dof_list[elements[i][0]][1], 
+    locel.append([dof_list[elements[i][0]][0], dof_list[elements[i][0]][1], 
                   dof_list[elements[i][0]][2], dof_list[elements[i][0]][3], 
                   dof_list[elements[i][0]][4], dof_list[elements[i][0]][5],
                   dof_list[elements[i][1]][0], dof_list[elements[i][1]][1], 
@@ -40,38 +40,15 @@ for i in range(len(elements)) :
                   dof_list[elements[i][1]][4], dof_list[elements[i][1]][5]])
 
 
-# Définition des constantes pour la structure
-E = 210e6                                           # Module de Young [Pa]
-A_leg = math.pi*(1 - (1-0.02)**2)                   # Section d'une poutre principale [m^2]
-A_beam = math.pi*(0.6**2 - (0.6-0.02)**2)           # Section d'une poutre secondaire [m¨2]
-nu = 0.3                                            # Coefficient de Poisson [-]
-rho = 7800                                          # Densité de l'acier utilisé [kg/m^3]
-G = E/(2*(1+nu))                                    # Module de cisaillement [Pa]
-h = 1
-
-# Définition des caractéristiques pour une poutre secondaire (selon les axes locaux)
-m_beam = rho*math.pi*h*(0.6**2 - (0.6-0.02**2))                    # Masse [kg]
-Iyz_beam = (math.pi/4)*(0.6**4 - (0.6-0.02)**2)                    # Moment quadratique selon l'axe y et z [m^4]
-Ix_beam = (math.pi/2)*(0.6**4 - (0.6-0.02)**2)                     # Moment quadratique selon l'axe x [m^4]
-Jx_beam = 0.5*m_beam*(0.6**2 + (0.6-0.02)**2)                      # Moment d'inertie selon l'axe x [kg.m^2]
-Jyz_beam = 0.25*m_beam*(0.6**2 + (0.6-0.02)**2)+(m_beam*(h**2))/12 # Moment d'inertie selon l'axe y et z [km.m^2]
-
-# Définition des caractéristiques pour une pour principale (elon les axes locaux)
-m_leg = rho*math.pi*h*(1**2 - (1-0.02)**2)                    # Masse d'une poutre principale [kg]
-Iyz_leg = (math.pi/4)*(1**4 - (1-0.02)**2)                    # Moment quadratique selon l'axe y et z [m^4]
-Ix_leg = (math.pi/2)*(1**4 - (1-0.02)**2)                     # Moment quadratique selon l'axe x [m^4]
-Jx_leg = 0.5*m_beam*(1**2 + (1-0.02)**2)                      # Moment d'inertie selon l'axe x [kg.m^2]
-Jyz_leg = 0.25*m_beam*(1**2 + (1-0.02)**2)+(m_beam*(h**2))/12 # Moment d'inertie selon l'axe y et z [kg.m^2]
-
-# Définition des constantes pour les rigid links
-rho_r = rho*1e-4    # Densité [kg/m^3]
-E_R = E*1e4         # Module de Young [Pa]
-A_r = A_leg*1e-2    # Section [m^2]
-Iyz_r = Iyz_leg*1e4 # Moment quadratique selon l'axe y et z [m^4]
-Jx_r = Jx_leg*1e4   # Moment d'intertie selon l'axe x [kg.m^2]
-
-
-
+# Création des matrices élémentaires, rotation et assemblage
+#boucle sur tous les éléments
+for i in range(len(elements)) : 
+    #création des matries élémentaires
+    param = fct.get_param(i, leg_elem, rili_elem, elements, nodes)
+    M_el,K_el = fct.elem_matrix(param)
+    #création de l'opérateur de rotation
+    # application de la rotation
+    # assemblage dans la matrice globale
 
 
 
